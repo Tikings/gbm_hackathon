d84a8277cea5e620dfffbb38bbc28f0ffeb0f5d1
Timothée SANCHEZ <timothee.sanchez.faly@gmail.com>
2025-03-20 11:54:11 +0100

    Fixed indentation

--- gbmhackathon/viz/visium_functions.py
+++ gbmhackathon/viz/visium_functions.py
@@ -118,4 +118,4 @@ def get_slide_img(slide_paths : pd.DataFrame, slide_idx : int, bucket : str = MO
     slide_path = str(slide_paths["path"].iloc[slide_idx])
     with io.BytesIO(s3.get_object(Bucket = bucket, Key = slide_path)["Body"].read()) as file_obj:
         slide = tiffslide.TiffSlide(file_obj)
-    return slide.get_thumbnail(slide.level_dimensions[-2])
+        return slide.get_thumbnail(slide.level_dimensions[-2])
