0cddc0764e3137f1a15d465727483f101d777c18
Timothée SANCHEZ <timothee.sanchez.faly@gmail.com>
2025-03-20 10:20:47 +0100

    Added folder pattern parser in s3_loader sublib
    - Aim to find the files that match a specific pattern in a given bucket
      folder

--- gbmhackathon/s3_loader.py
+++ gbmhackathon/s3_loader.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+import boto3
+import re
 
 MOSAIC_DATASET = "ABSTRA_DATASET_03bb30aa_16ed_4b89_913e_fe009db2aabd"
 BRUCE_DATESET = "ABSTRA_DATASET_8bfd41bf_a110_4748_bda1_8c225cdde6b5"
@@ -11,3 +13,21 @@ def get_s3_dataset_info(dataset_name : str = MOSAIC_DATASET):
     s3_bucket, s3_folder = s3_link.split("/")[2], Path(s3_link.split("/")[3])
     return s3_bucket, s3_folder
 
+def lister_fichiers_s3(bucket_name, pattern= "*" ):
+
+    s3 = boto3.client("s3")
+    regex = re.compile(pattern)
+
+    try:
+        response = s3.list_objects_v2(Bucket=bucket_name)
+
+        if "Contents" in response:
+            fichiers = [obj["Key"] for obj in response["Contents"] if regex.match(obj["Key"])]
+            return fichiers
+        else:
+            # No file matching the pattern found
+            return []
+
+    except Exception as e:
+        print(f"Error : {e}")
+        return []
