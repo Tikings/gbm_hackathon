7a7c1312e5a5fc5787d826d88cfd815cdf9bc53a
Timothée SANCHEZ <timothee.sanchez.faly@gmail.com>
2025-03-21 15:23:55 +0100

    Submodule for HE tif files management
    - Added function to load the TiffSlide obj

---
+++ gbmhackathon/utils/he_functions.py
@@ -0,0 +1,18 @@
+import io
+import gc
+import tiffslide
+import pandas as pd
+import boto3
+from gbmhackathon.definitions import MOSAIC_BUCKET
+
+def get_slide_img(
+    slide_paths: pd.DataFrame, slide_idx: int, bucket: str = MOSAIC_BUCKET
+):
+    s3 = boto3.client("s3")
+    slide_path = str(slide_paths["path"].iloc[slide_idx])
+    buffer = io.BytesIO(s3.get_object(Bucket=bucket, Key=slide_path)["Body"].read())
+    with buffer as file_obj:
+        slide = tiffslide.TiffSlide(file_obj)
+    del buffer
+    gc.collect()
+    return slide
