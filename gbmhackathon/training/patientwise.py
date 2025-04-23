4cb09f86b6615b6b91e0c40428f891a1aba61fe7
Timothée SANCHEZ <timothee.sanchez.faly@gmail.com>
2025-04-23 23:14:57 +0200

    FIX minor bug for loader

--- gbmhackathon/training/patientwise.py
+++ gbmhackathon/training/patientwise.py
@@ -8,7 +8,7 @@ from torch.utils.data import Dataset
 import re
 from typing import List, Dict
 
-ABSTRA_PROJECT_STORAGE_BUCKET = "s3://abstra-project-storage-lttemftb/1b75dc89-ad27-4a65-9e7f-877d1b4f36fc/"
+ABSTRA_PROJECT_STORAGE_BUCKET = "s3://abstra-project-storage-lttemftb/1b75dc89-ad27-4a65-9e7f-877d1b4f36fc"
 PATTERN_PATIENT = "(HK_G_[0-9]{3}(a|b))"
 class PatientLearningDataset(Dataset):
     def __init__(self,
@@ -24,7 +24,7 @@ class PatientLearningDataset(Dataset):
         for key in self.name_emb.keys(): 
             self.dict_emb[key] = self._load_pickle(
                 self.root, 
-                self.folder_name,
+                self.folder,
                 self.name_emb[key])
             if key == "clinical" : 
                 self.dict_emb[key] = self._load_pickle(
@@ -40,11 +40,11 @@ class PatientLearningDataset(Dataset):
         patients = list(set_patient) 
 
         index_patient = list(range(len(patients)))
-        self.ind2patient = dict(zip(index_patient, self.patients))
+        self.ind2patient = dict(zip(index_patient, patients))
 
     @staticmethod
     def _load_pickle(self, root, folder_name,  emb_name):
-        path = str(root / folder_name / emb_name)
+        path = str(root + "/" + folder_name + "/" + emb_name)
         dict_patient = load_s3(path)
         formated_dict = self.format_dict_keys(dict_patient, PATTERN_PATIENT)
         return formated_dict
