97a43791f879b95a067c71b89f1b324436f42f36
AdrienBouchet1 <adrien.bouchet@agroparistech.fr>
2025-05-04 11:25:11 +0200

    tests

--- gbmhackathon/utils/experiments.py
+++ gbmhackathon/utils/experiments.py
@@ -19,6 +19,7 @@ class training_experiment :
             self.config=config
             self.experiment_config=config["Experiments"]
             self.__setup_output_folder()
+            self.result=dict()
 
 
 
@@ -38,6 +39,8 @@ class training_experiment :
 
         with open(os.path.join(self.experiment_dir,"config.json"), "w") as f:
             json.dump(self.config, f)
+        
+        
 
 
     def Multiple_run_experiment(self) : 
@@ -67,7 +70,10 @@ class training_experiment :
         for run in range(len(list(list_run_loss.keys()))):
             plt.plot(range(1, epochs+1), list_run_loss[run], linewidth=1, c='grey', label=f'Run {run+1}')
 
+        
         avg_curve = np.mean(np.stack(list(list_run_loss.values()), axis=0), axis=0)
+        self.result["MultiRunResult"]={"list_run_loss" : list_run_loss,"average_loss" : avg_curve}
+        
         plt.plot(range(1, epochs+1), avg_curve, linewidth=3, label='Average', zorder=10)   
         plt.xlabel('Epoch')
         plt.ylabel('Loss')
@@ -76,6 +82,7 @@ class training_experiment :
         plt.grid(True)
         fig=plt.gcf()
         fig.savefig(os.path.join(exp_folder,'loss_curves.pdf'),dpi=1000)
+
 
 
                 
@@ -89,6 +96,7 @@ class model_comparisons :
         overrides should be a dictionary with the format "overrides_name : dict"
         """
         self.original_config=config
+        self.__setup_output_folder()
         self.overrided_config=self.instantiate_overrides_config(overrides)
 
 
@@ -116,10 +124,53 @@ class model_comparisons :
 
     def test_rapidos(self) : 
          
-         for override,config in self.overrided_config.items(): 
-              print("override {}!!! ".format(override))
-              wahou=global_wrapper.MME_Global(config)
-              wahou.fit_mme()
+        for override,config in self.overrided_config.items(): 
+              config["global_settings"]["output_dir"]=os.path.join(config["global_settings"]["output_dir"],override)
+              exp=training_experiment(config=config)
+              exp.Multiple_run_experiment()
+              avg_loss=dict()
+              avg_loss[override]=exp.result["MultiRunResult"]["average_loss"]
+
+
+        max_avg_len=np.max([len(i) for i in avg_loss.values()])
+        plt.figure(figsize=(8, 5))
+
+        for conf in avg_loss.keys():
+            tab=avg_loss[conf]
+            if tab.shape[0] < max_avg_len : 
+                tab=np.hstack((tab,np.full((max_avg_len-tab.shape[0]),fill_value=np.nan)))
+            
+            plt.plot(range(1, max_avg_len+1), tab, linewidth=1, label=" config : {}".format(conf))
+        
+        plt.savefig(os.path.join(self.experiment_dir,"comparison_loss.pdf"),dpi=1000)
+
+
+
+      
+    def __setup_output_folder(self) : 
+
+        """
+        """
+
+        output_folder=self.original_config["global_settings"]["output_dir"]
+        assert os.path.exists(output_folder), "The output folder specified does not exist"
+
+        name_folder="experiment_{}".format(datetime.now().strftime("%Y-%m-%d_%H-%M"))
+
+        os.makedirs(os.path.join(output_folder,name_folder))
+        
+        self.experiment_dir=os.path.join(output_folder,name_folder)
+        
+
+        with open(os.path.join(self.experiment_dir,"config.json"), "w") as f:
+            json.dump(self.original_config, f)
+
+        with open(os.path.join(self.experiment_dir,"overrided_config.json"), "w") as f:
+            json.dump(self.overrided_config, f)
+        
+
+        
+        
 
 
 
